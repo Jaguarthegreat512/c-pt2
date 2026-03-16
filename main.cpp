a2520668853fb3ca97f2e6e295ef6b0e0ce07718
Jude <judethedude0512@outlook.com>
2026-03-16 10:41:19 -0400

    c++ begining pt2

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Reads `count` CO2 values from stdin and returns their average.
+double readAverageCO2(int count) {
+    double sum = 0.0;
+    for (int i = 1; i <= count; i++) {
+        double co2;
+        cout << "Enter CO2 level #" << i << " (ppm): ";
+        cin >> co2;
+        sum += co2;
+    }
+    return sum / count;
+}
+
+int main() {
+    cout << "Whats your name?: ";
+
+    string name;
+    getline(cin, name);
+
+    cout << "Hello, " << name << "!" << endl;
+
+    double average = readAverageCO2(3);
+    cout << "Average CO2 level: " << average << " ppm" << endl;
+
+    if (average > 1000) {
+        cout << "Turning on filtration so u wont die." << endl;
+    } else {
+        cout << "Yay ur not gonna die." << endl;
+    }
+
+    return 0;
+}
